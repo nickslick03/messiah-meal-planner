2f45745d40d2b745905ef99b0c419d96abc374b0
cdrice26 <caleb.rice.2626@gmail.com>
2024-06-12 19:18:16 -0400

    Added to mapUserMeals test

--- src/test/mapUserMeals.test.ts
+++ src/test/mapUserMeals.test.ts
@@ -2,11 +2,84 @@ import mapUserMeals from '../lib/mapUserMeals';
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { v4 as uuid } from 'uuid';
 import { Weekday } from '../types/userSelectedMealsObject';
+import dereferenceMeal from '../lib/dereferenceMeal';
+import Meal from '../types/Meal';
 
 // Mocking the uuid function to return predictable values
 vi.mock('uuid', () => ({
   v4: vi.fn()
 }));
+
+const meals = [
+  { id: '1', instanceId: '1', name: 'Lunch', price: 8.99, location: 'Lottie' },
+  {
+    id: '2',
+    instanceId: '2',
+    name: 'Dinner',
+    price: 12.99,
+    location: 'Lottie'
+  },
+  { id: '3', instanceId: '3', name: 'Lunch', price: 8.99, location: 'Lottie' },
+  {
+    id: '4',
+    instanceId: '4',
+    name: 'Dinner',
+    price: 12.99,
+    location: 'Lottie'
+  },
+  { id: '5', instanceId: '5', name: 'Lunch', price: 8.99, location: 'Lottie' },
+  {
+    id: '6',
+    instanceId: '6',
+    name: 'Dinner',
+    price: 12.99,
+    location: 'Lottie'
+  },
+  { id: '7', instanceId: '7', name: 'Lunch', price: 8.99, location: 'Lottie' },
+  {
+    id: '8',
+    instanceId: '8',
+    name: 'Dinner',
+    price: 12.99,
+    location: 'Lottie'
+  },
+  { id: '9', instanceId: '9', name: 'Lunch', price: 8.99, location: 'Lottie' },
+  {
+    id: '10',
+    instanceId: '10',
+    name: 'Dinner',
+    price: 12.99,
+    location: 'Lottie'
+  },
+  {
+    id: '11',
+    instanceId: '11',
+    name: 'Lunch',
+    price: 8.99,
+    location: 'Lottie'
+  },
+  {
+    id: '12',
+    instanceId: '12',
+    name: 'Dinner',
+    price: 12.99,
+    location: 'Lottie'
+  },
+  {
+    id: '13',
+    instanceId: '13',
+    name: 'Lunch',
+    price: 8.99,
+    location: 'Lottie'
+  },
+  {
+    id: '14',
+    instanceId: '14',
+    name: 'Dinner',
+    price: 12.99,
+    location: 'Lottie'
+  }
+];
 
 const selectedDays = [0, 1, 2, 3, 4, 5, 6];
 const userSelectedMeals = {
@@ -52,6 +125,23 @@ const mealQueue = {
   ]
 };
 
+const customMeals = {
+  value: [
+    {
+      id: '20',
+      location: 'Lottie',
+      name: 'Lunch',
+      price: 8.99
+    } as Meal,
+    {
+      id: '21',
+      location: 'Lottie',
+      name: 'Dinner',
+      price: 12.99
+    } as Meal
+  ]
+};
+
 describe('mapUserMeals', () => {
   beforeEach(() => {
     // Reset the mocked uuid function before each test
@@ -62,7 +152,134 @@ describe('mapUserMeals', () => {
     expect(Object.keys(mapUserMeals((day) => [day, []])).length).toBe(7);
   });
 
-  it('should return an object with the correct keys and values', () => {
+  it('should return an object with the correct keys and values when used in dayEditor', () => {
+    const result = mapUserMeals(
+      (day: Weekday) =>
+        [
+          day,
+          userSelectedMeals.value[day]
+            .map((mr) => dereferenceMeal(mr, meals, customMeals.value))
+            .filter((m) => m !== undefined)
+        ] as [string, Meal[]]
+    );
+
+    expect(result).toEqual({
+      Sunday: [
+        {
+          id: '13',
+          instanceId: '13',
+          name: 'Lunch',
+          price: 8.99,
+          location: 'Lottie'
+        },
+        {
+          id: '14',
+          instanceId: '14',
+          name: 'Dinner',
+          price: 12.99,
+          location: 'Lottie'
+        }
+      ],
+      Monday: [
+        {
+          id: '1',
+          instanceId: '1',
+          name: 'Lunch',
+          price: 8.99,
+          location: 'Lottie'
+        },
+        {
+          id: '2',
+          instanceId: '2',
+          name: 'Dinner',
+          price: 12.99,
+          location: 'Lottie'
+        }
+      ],
+      Tuesday: [
+        {
+          id: '3',
+          instanceId: '3',
+          name: 'Lunch',
+          price: 8.99,
+          location: 'Lottie'
+        },
+        {
+          id: '4',
+          instanceId: '4',
+          name: 'Dinner',
+          price: 12.99,
+          location: 'Lottie'
+        }
+      ],
+      Wednesday: [
+        {
+          id: '5',
+          instanceId: '5',
+          name: 'Lunch',
+          price: 8.99,
+          location: 'Lottie'
+        },
+        {
+          id: '6',
+          instanceId: '6',
+          name: 'Dinner',
+          price: 12.99,
+          location: 'Lottie'
+        }
+      ],
+      Thursday: [
+        {
+          id: '7',
+          instanceId: '7',
+          name: 'Lunch',
+          price: 8.99,
+          location: 'Lottie'
+        },
+        {
+          id: '8',
+          instanceId: '8',
+          name: 'Dinner',
+          price: 12.99,
+          location: 'Lottie'
+        }
+      ],
+      Friday: [
+        {
+          id: '9',
+          instanceId: '9',
+          name: 'Lunch',
+          price: 8.99,
+          location: 'Lottie'
+        },
+        {
+          id: '10',
+          instanceId: '10',
+          name: 'Dinner',
+          price: 12.99,
+          location: 'Lottie'
+        }
+      ],
+      Saturday: [
+        {
+          id: '11',
+          instanceId: '11',
+          name: 'Lunch',
+          price: 8.99,
+          location: 'Lottie'
+        },
+        {
+          id: '12',
+          instanceId: '12',
+          name: 'Dinner',
+          price: 12.99,
+          location: 'Lottie'
+        }
+      ]
+    });
+  });
+
+  it('should return an object with the correct keys and values when used in mealQueue', () => {
     // Mocking the uuid function to return specific values in sequence
     vi.mocked(uuid)
       .mockReturnValueOnce('mock-uuid-1')
@@ -135,9 +352,5 @@ describe('mapUserMeals', () => {
         { id: '16', instanceId: 'mock-uuid-14' }
       ]
     });
-
-    // Further debugging output if needed
-    expect(result).toHaveProperty('Monday');
-    expect(result.Monday).toHaveLength(4);
   });
 });
