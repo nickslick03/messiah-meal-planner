6155f06030f94f5b308f5d2c413e919fd1cb5e12
nickslick03 <nicholasepps03@gmail.com>
2024-07-10 18:56:25 -0400

    re centered and brought notification to front

--- src/components/other/Notification.tsx
+++ src/components/other/Notification.tsx
@@ -22,7 +22,7 @@ const Notification = ({
         <div
             ref={div} 
             className={`${message.text === '' ? 'hidden' : ''} 
-            fixed top-0 w-full md:w-3/5 flex justify-center z-10 -translate-y-full animate-topNotify`}>
+            fixed top-0 w-full md:w-3/5 flex justify-center z-20 -translate-y-full animate-topNotify`}>
             <div className="w-full bg-messiah-light-blue drop-shadow-dark p-2 my-4 mx-4 md:mx-0 rounded 
             text-center text-lg font-semibold bg-opacity-80 backdrop-blur-sm">
                 {message.text}
