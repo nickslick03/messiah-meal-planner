f09be6aa0df6a22578c86f0dfd797216bc5b1d79
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2024-09-09 22:49:48 -0400

    Updated release notes

--- src/static/releaseNotes.ts
+++ src/static/releaseNotes.ts
@@ -8,7 +8,7 @@ const releaseNotes: ReleaseNoteType[] = [
   },
   {
     versionNumber: '0.0.1',
-    bugFixes: [],
+    bugFixes: ['Fixed an issue where invalid recipes would crash the app.'],
     enhancements: [
       'Updated Union, Falcon, and Vending prices for 2024-25 school year.'
     ]
