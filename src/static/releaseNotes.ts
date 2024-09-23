22389cf68b214a18484969ee8709294a68cb8f37
Nicholas Epps <nicholasepps03@gmail.com>
2024-09-23 18:57:13 -0400

    added release notes

--- src/static/releaseNotes.ts
+++ src/static/releaseNotes.ts
@@ -18,7 +18,8 @@ const releaseNotes: ReleaseNoteType[] = [
     versionNumber: '0.0.2',
     bugFixes: [
       'Fixed an issue where all meals would trigger the error highlighting in the day editor.',
-      'Fixed issue where tutorial would cover up the notifications.'
+      'Fixed issue where tutorial would cover up the notifications.',
+      'Fixed meal table overflow'
     ],
     enhancements: []
   }
