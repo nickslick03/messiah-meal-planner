317b904af7afd0b298b27bff3a5420920e358be5
cdrice26 <156816748+cdrice26@users.noreply.github.com>
2024-09-25 21:06:02 -0400

    Updated release notes

--- src/static/releaseNotes.ts
+++ src/static/releaseNotes.ts
@@ -17,6 +17,7 @@ const releaseNotes: ReleaseNoteType[] = [
   {
     versionNumber: '0.0.2',
     bugFixes: [
+      'Fixed initial positioning of switches',
       'Fixed the price of Union Acai bowls',
       'Fixed an issue where all meals would trigger the error highlighting in the day editor.'
     ],
